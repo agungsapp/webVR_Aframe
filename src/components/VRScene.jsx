6d1f7917a346a0710836975b30a281cd5e3d8031
agungsapp <agung.dni19@gmail.com>
2024-08-05 21:31:05 +0700

    penambahan mode VR

--- src/components/VRScene.jsx
+++ src/components/VRScene.jsx
@@ -39,47 +39,74 @@ const VRScene = () => {
     }, 300);
   };
 
+  useEffect(() => {
+    const vrButton = document.getElementById('vr-button');
+    vrButton.addEventListener('click', () => {
+      const sceneEl = document.querySelector('a-scene');
+      if (sceneEl) {
+        sceneEl.enterVR();
+      }
+    });
+  }, []);
+
   return (
-    <a-scene>
-      <a-assets>
-        <img id="top_right" src="Left on Table_L.png" />
-        <img id="bottom_center" src="behind_L.png" />
-        <img id="top_left" src="Right on Door_R.png" />
-        <img id="top_center" src="Center on board_L_R.png" />
-      </a-assets>
+    <>
+      <a-scene vr-mode-ui="enabled: true">
+        <a-assets>
+          <img id="top_right" src="Left on Table_L.png" />
+          <img id="bottom_center" src="behind_L.png" />
+          <img id="top_left" src="Right on Door_R.png" />
+          <img id="top_center" src="Center on board_L_R.png" />
+        </a-assets>
 
-      <a-sky
-        id="image-360"
-        radius="30"
-        src={currentImage}
-        animation__fade="property: material.color; type: color; from: #FFF; to: #000; dur: 300; startEvents: fade"
-        animation__fadeback="property: material.color; type: color; from: #000; to: #FFF; dur: 300; startEvents: fadeback"
-      />
-
-      <a-camera>
-        <a-cursor
-          id="cursor"
-          animation__click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150"
-          animation__fusing="property: scale; startEvents: fusing; from: 1 1 1; to: 0.1 0.1 0.1; dur: 1500"
+        <a-sky
+          id="image-360"
+          radius="30"
+          src={currentImage}
+          animation__fade="property: material.color; type: color; from: #FFF; to: #000; dur: 300; startEvents: fade"
+          animation__fadeback="property: material.color; type: color; from: #000; to: #FFF; dur: 300; startEvents: fadeback"
         />
-      </a-camera>
 
-      {hotspots[currentImage].map((hotspot, index) => (
-        <a-entity
-          key={index}
-          geometry="primitive: sphere; radius: 0.2"
-          material="color: #FF0000; opacity: 0.8"
-          position={hotspot.position}
-          look-at="[camera]"
-          event-set__mouseenter="_event: mouseenter; material.color: #FFFF00"
-          event-set__mouseleave="_event: mouseleave; material.color: #FF0000"
-          onClick={() => handleHotspotClick(hotspot.target)}
-        >
-          <a-animation attribute="scale" from="1 1 1" to="1.2 1.2 1.2" dur="500" direction="alternate" repeat="indefinite"></a-animation>
-        </a-entity>
-      ))}
-    </a-scene>
+        <a-camera>
+          <a-cursor
+            id="cursor"
+            animation__click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150"
+            animation__fusing="property: scale; startEvents: fusing; from: 1 1 1; to: 0.1 0.1 0.1; dur: 1500"
+          />
+        </a-camera>
+
+        {hotspots[currentImage].map((hotspot, index) => (
+          <a-entity
+            key={index}
+            geometry="primitive: sphere; radius: 0.2"
+            material="color: #FF0000; opacity: 0.8"
+            position={hotspot.position}
+            look-at="[camera]"
+            event-set__mouseenter="_event: mouseenter; material.color: #FFFF00"
+            event-set__mouseleave="_event: mouseleave; material.color: #FF0000"
+            onClick={() => handleHotspotClick(hotspot.target)}
+          >
+            <a-animation attribute="scale" from="1 1 1" to="1.2 1.2 1.2" dur="500" direction="alternate" repeat="indefinite"></a-animation>
+          </a-entity>
+        ))}
+      </a-scene>
+
+      <button id="vr-button" style={buttonStyle}>Enter VR</button>
+    </>
   );
+};
+
+const buttonStyle = {
+  position: 'absolute',
+  bottom: '20px',
+  left: '50%',
+  transform: 'translateX(-50%)',
+  padding: '10px 20px',
+  fontSize: '16px',
+  backgroundColor: '#FFF',
+  border: 'none',
+  borderRadius: '5px',
+  cursor: 'pointer',
 };
 
 export default VRScene;
